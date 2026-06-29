74405a36e3d641eccf5d996908eb5c9fbf0d5de9
Syed Sami Ul Hassan <samiulhassan1705@gmail.com>
2026-06-29 04:57:14 -0700

    Create TOUPLE _PROBLEM.py

---
+++ DAY2/Practice_Problems/TOUPLE _PROBLEM.py
@@ -0,0 +1,15 @@
+// Count occurrences of an element
+num=(20,43,55,12,43,65,3,2,1,20)
+count=num.count(20)
+print(count)
+
+
+// Convert a tuple into a list and vice versa
+num=[12,43,21,54,32,65]
+newtuple=tuple(num)
+print(newtuple)
+print(type(newtuple))
+
+newlist= list(newtuple)
+print(newlist)
+print(type(newlist))
