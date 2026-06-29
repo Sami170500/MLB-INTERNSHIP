8128b05d857d2cf26ad439d6d81dcd1c765da37d
Syed Sami Ul Hassan <samiulhassan1705@gmail.com>
2026-06-29 04:59:46 -0700

    Create SET_PROBLEM.py

---
+++ DAY2/Practice_Problems/SET_PROBLEM.py
@@ -0,0 +1,14 @@
+// Find unique values from a list
+num1=[20,20,20,43,21,32,43,44,54,33,32]
+U_num=set(num1)
+print(U_num)
+
+
+// Perform union and intersection operations
+num1={21,32,21,32,43,11,2,1}
+num2={21,32,12,32,43,11,2,4,}
+union=num1|num2
+print(union)
+
+intersection=num1&num2
+print(intersection)
