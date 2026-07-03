5506be65c12591cb56a6d72bf58d63cd1f4e8f35
Syed Sami Ul Hassan <samiulhassan1705@gmail.com>
2026-07-03 03:37:40 -0700

    Implement data analysis for student performance
    Added data analysis operations on student performance data, including reading the CSV file, displaying basic statistics, and calculating average scores.

--- Day4/Practice/pandas.py
+++ Day4/Practice/pandas.py
@@ -1,1 +1,13 @@
-
+import pandas as pd
+df=pd.read_csv("StudentsPerformance.csv")
+print(df)
+print(df.head())
+print(df.tail())
+print(df.info())
+print(df.describe())
+print(df.isnull().sum())
+print(df[df['math score']>75])
+df["Average"]=(df['math score']+df['reading score']+df['writing score'])/3
+print(df['Average'])
+print(df[df["gender"]=='male'])
+print(df[df['math score']<df['Average']])
