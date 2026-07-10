72ecf2a957d6e7cb313b4a7b8f4e29ab38616bc2
Syed Sami Ul Hassan <samiulhassan1705@gmail.com>
2026-07-10 03:08:44 -0700

    Create k-means.py

---
+++ DAY9/k-means.py
@@ -0,0 +1,24 @@
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import seaborn as sns
+wcss=[]
+for i in range(1,11):
+    kmeans=KMeans(n_clusters=i,random_state=42)
+    kmeans.fit(df)
+    wcss.append(kmeans.inertia_)
+
+print(wcss)    
+
+kmeans=KMeans(n_clusters=3,random_state=42)
+df['clusters']=kmeans.fit_predict(df)
+print(df.head())
+
+sns.scatterplot(
+    data=df,
+    x="sepal length (cm)",
+    y="sepal width (cm)",
+    hue="clusters"
+)
+
+plt.title("K-Means Clustering")
+plt.show()
